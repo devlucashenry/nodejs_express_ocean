74b9af209a185df65498ed2c08a6d96786b1771e
devlucashenry <142762523+devlucashenry@users.noreply.github.com>
2026-02-09 23:17:19 +0000

    endpoint personagens

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.get('/oi', (req, res) => {
   res.send('Olá Mundo no Front!');
 });
 
+const lista = ['Rick Sanchez', 'Morty Smith', 'Beth Smith']
+
+app.get('/personagens', (req, res) => {
+  res.send(lista)
+})
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta: ${port}`)
 })
